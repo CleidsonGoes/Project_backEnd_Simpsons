fd3ed6f7e049b8af74e8fd80bd50cef6968c39dc
CleidsonGoes <jorgegoes4@gmail.com>
2024-07-17 11:38:04 -0300

    camada serviço para personagens criada

--- src/services/character.ts
+++ src/services/character.ts
@@ -1,14 +1,15 @@
-import Cast from '../interfaces/cast';
+import Character from '../interfaces/character';
 import { SimpleModel } from '../models/model';
-import CastModel from '../models/cast';
+import CharacterModel from '../models/character';
 import Service from './service';
 import BadRequest from '../errors/badRequest';
-export class CastService extends Service<Cast> {
-  constructor(model: SimpleModel<Cast> = new CastModel()) {
+
+export class CharacterService extends Service<Character> {
+  constructor(model: SimpleModel<Character> = new CharacterModel()) {
     super(model);
   }
 
-  async create(obj: Cast): Promise<void> {
+  async create(obj: Character): Promise<void> {
     if (obj.name.length <= 3) {
       throw new BadRequest('O nome precisa ter pelo menos 4 caracteres');
     }
