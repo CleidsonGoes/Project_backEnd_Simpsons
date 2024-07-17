2a60605a0e01eea88447c9029a8a7daf15ed128b
CleidsonGoes <jorgegoes4@gmail.com>
2024-07-17 14:28:20 -0300

    rotas de cada recurso criadas e usadas c/ estratégia de barrel

---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+import castRouter from './cast';
+import castCharacterRouter from './castCharacter';
+import characterRouter from './character';
+
+export { characterRouter, castCharacterRouter, castRouter };
