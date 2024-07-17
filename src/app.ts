f6a9dfd1c2f8c323a0f24045220dbdcdc567df3c
CleidsonGoes <jorgegoes4@gmail.com>
2024-07-17 19:06:43 -0300

    middleware de erro criado

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import * as routers from './routes';
+import BaseHTTPError from './errors/httpError';
 
 
 const app = express();
@@ -12,5 +13,15 @@ app.get('/health', (req, res) => {
 app.use('/characters', routers.characterRouter);
 app.use('/casts', routers.castRouter);
 app.use('/castCharacters', routers.castCharacterRouter);
+
+app.use((err: BaseHTTPError, _: Request, res: Response, __: NextFunction) => {
+    if (err.statusCode) {
+      return res.status(err.statusCode).json({ message: err.message });
+    }
+    // eslint-disable-next-line no-console
+    console.error(err.message);
+    return res.status(500).json({ message: 'Erro interno' });
+  });
+
 
 export default app;
